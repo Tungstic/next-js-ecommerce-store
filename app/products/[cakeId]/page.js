927a3f0bd40507220d90d423d30d59da5df03392
Natalia Budulchieva <nbudulchieva@gmail.com>
2023-05-20 21:05:20 +0200

    Add styles to each cake page

--- app/products/[cakeId]/page.js
+++ app/products/[cakeId]/page.js
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import { notFound } from 'next/navigation';
 import { getCakeById } from '../../../database/cakes';
+import styles from './page.module.scss';
 
 export const dynamic = 'force-dynamic';
 
@@ -14,16 +15,22 @@ export default function CakePage({ params }) {
   }
 
   return (
-    <main>
-      <h1>{`${singleCake.name} cake`}</h1>
-      <Image
-        data-test-id="product-image"
-        src={`/${singleCake.name}.jpg`}
-        width={200}
-        height={300}
-      />
-      this is a page for the {singleCake.name} cake
-      <div data-test-id="product-price">{`price: €${singleCake.price}`}</div>
+    <main className={styles.cakeCard}>
+      <div>
+        <h1>{`${singleCake.name} cake`}</h1>
+        <Image
+          data-test-id="product-image"
+          src={`/${singleCake.name}.jpg`}
+          width={200}
+          height={300}
+        />
+      </div>
+      <div className={styles.description}>
+        <div>this is a page for the {singleCake.name} cake</div>
+        <div data-test-id="product-price">{`price: €${singleCake.price}`}</div>
+        <div>Quantity</div>
+        <button>Add to cart</button>
+      </div>
     </main>
   );
 }
