aade151574d7fad145d49c40c0f934330a60a963
Natalia Budulchieva <nbudulchieva@gmail.com>
2023-05-21 17:13:41 +0200

    Use cookie-array on CartPage to display order

--- app/cart/page.js
+++ app/cart/page.js
@@ -1,11 +1,25 @@
+import { getCookie } from '../../util/cookies';
+import { parseJson } from '../../util/json';
+
 export default function CartPage() {
+  // get all current cakes added to cart as a cookie
+  const cakeQuantityCookie = getCookie('nameOfCookie');
+
+  // get the same as an array of objects
+
+  const cakeQuantities = !cakeQuantityCookie
+    ? []
+    : parseJson(cakeQuantityCookie);
+
+  console.log(cakeQuantities);
+
   return (
     <main>
       <h1>Shopping cart</h1>
       <ul>
         <li>
-          Cake, price
-          <div>Quantity</div>
+          Cake, quantity
+          <div>{JSON.stringify(cakeQuantities)}</div>
           <button>Remove</button>
         </li>
       </ul>
