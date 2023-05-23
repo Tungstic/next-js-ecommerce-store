abea1fb23e14b2a347ec2d65467bfed8a96f521b
Natalia Budulchieva <nbudulchieva@gmail.com>
2023-05-23 18:06:35 +0200

    Create a rough structure and style for CheckoutPage

--- app/checkout/page.js
+++ app/checkout/page.js
@@ -1,3 +1,56 @@
+import styles from './page.module.scss';
+
 export default function CheckoutPage() {
-  return <main>Payment</main>;
+  return (
+    <main>
+      <div>Your total is: total</div>
+      <div>
+        Please fill out and submit the form by clicking "Confirm order" button
+      </div>
+
+      <form className={styles.info}>
+        <label>
+          First name
+          <input />
+        </label>
+        <label>
+          Last name
+          <input />
+        </label>
+        <label>
+          Email
+          <input />
+        </label>
+        <label>
+          Address
+          <input />
+        </label>
+        <label>
+          City
+          <input />
+        </label>
+        <label>
+          Postal code
+          <input />
+        </label>
+        <label>
+          Country
+          <input />
+        </label>
+        <label>
+          Credit card
+          <input />
+          <label>
+            Expiration date
+            <input />
+          </label>
+          <label>
+            Security code
+            <input />
+          </label>
+        </label>
+        <button>Confirm order</button>
+      </form>
+    </main>
+  );
 }
