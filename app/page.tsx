bc402a29989a78567d7989547d7fd5e0cfaf9e0e
Natalia Budulchieva <nbudulchieva@gmail.com>
2023-06-06 17:54:58 +0200

    Add unoptimized attr to main image for deployment

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,7 @@ export default function HomePage() {
           priority={true}
           width={600}
           height={400}
+          unoptimized={true}
         />
         <p>
           ... where every bite is a celebration of flavor and artistry. Indulge
