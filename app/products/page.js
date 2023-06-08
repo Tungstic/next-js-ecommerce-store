536765e0df0b9bb5e210b2a0d88a2b6c1fca22a5
Natalia Budulchieva <nbudulchieva@gmail.com>
2023-06-08 16:58:37 +0200

    Revert to having images and links wrapped in div

--- app/products/page.js
+++ app/products/page.js
@@ -14,24 +14,23 @@ export default async function ProductsPage() {
       <div className={styles.gridOfCakes}>
         {cakes.map((cake) => {
           return (
-            // <div className={styles.oneCake} key={`cake-div-${cake.id}`}>
-            <Image
-              src={`/${cake.name}.avif`}
-              priority
-              unoptimized
-              placeholder="blur"
-              width={200}
-              height={300}
-              alt={`photo of ${cake.name} cake`}
-              key={`cake-${cake.id}`}
-            />
-            /*  <Link
+            <div className={styles.oneCake} key={`cake-div-${cake.id}`}>
+              <Image
+                src={`/${cake.name}.avif`}
+                priority
+                unoptimized
+                placeholder="blur"
+                width={200}
+                height={300}
+                alt={`photo of ${cake.name} cake`}
+              />
+              <Link
                 href={`/products/${cake.id}`}
                 data-test-id={`product-${cake.id}`}
               >
                 {cake.name}
               </Link>
-            </div> */
+            </div>
           );
         })}
       </div>
