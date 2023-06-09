e5edea2fe5ae3537b36316b76e69354e376f0231
Natalia Budulchieva <nbudulchieva@gmail.com>
2023-06-09 15:06:57 +0200

    Make input value typable instead of read-only

--- app/products/[cakeId]/AddToCart.tsx
+++ app/products/[cakeId]/AddToCart.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
 import { addCakeToCart } from './actions';
 import styles from './addToCart.module.scss';
 
@@ -15,34 +15,44 @@ export default function AddToCart(props: Props) {
   const router = useRouter();
 
   function incrementCake() {
-    if (quantity >= 10) {
-      return quantity;
-    }
     setQuantity(quantity + 1);
   }
   function decrementCake() {
     if (quantity <= 1) {
-      return quantity;
+      return;
     }
     setQuantity(quantity - 1);
   }
 
+  /*   function typeInQuantity(event: FormEvent<HTMLInputElement>) {
+    if (Number(event.currentTarget.value) >= 1) {
+      setQuantity(Number(event.currentTarget.value));
+    }
+  } */
+
   return (
     <>
       <label className={styles.moreOrLessCake}>
-        Quantity (max. 10)
-        <button aria-label="Add 1 to cake quantity" onClick={incrementCake}>
+        Quantity
+        <button
+          aria-label="Add 1 to cake quantity"
+          onClick={() => incrementCake()}
+        >
           +
         </button>
         <input
-          readOnly
           name="show-quantity"
           data-test-id="product-quantity"
           value={quantity}
+          onChange={(event) => {
+            Number(event.currentTarget.value) >= 0
+              ? setQuantity(Number(event.currentTarget.value))
+              : setQuantity(1);
+          }}
         />
         <button
           aria-label="Subtract 1 from cake quantity"
-          onClick={decrementCake}
+          onClick={() => decrementCake()}
         >
           -
         </button>
