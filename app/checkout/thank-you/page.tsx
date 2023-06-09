e6a84563d6d5528ebc451290df0d3fb84e14170e
Natalia Budulchieva <nbudulchieva@gmail.com>
2023-06-09 16:59:13 +0200

    Add metadata to thank-you page

--- app/checkout/thank-you/page.tsx
+++ app/checkout/thank-you/page.tsx
@@ -1,3 +1,8 @@
+export const metadata = {
+  title: 'Thank you for your order',
+  description: 'Your order has been received',
+};
+
 export default function ThankYouPage() {
   return <main>Thank you for your order</main>;
 }
